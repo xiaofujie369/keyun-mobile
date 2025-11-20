ba5c8f6bc1bb1737da11b40eaabb7b27c7f9126a
xiaofujie369 <132354004+xiaofujie369@users.noreply.github.com>
2025-11-21 06:06:19 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,1 +1,15 @@
-import { createRouter, createWebHashHistory } from 'vue-router'; const routes=[{path:'/login',component:()=>import('../pages/Login.vue')},{path:'/dashboard',component:()=>import('../pages/Dashboard.vue')}]; export default createRouter({history:createWebHashHistory(),routes});
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+  { path: '/', redirect: '/dashboard' },
+  { path: '/login', component: () => import('../pages/Login.vue') },
+  { path: '/dashboard', component: () => import('../pages/Dashboard.vue') },
+  { path: '/nodes', component: () => import('../pages/Nodes.vue') },
+  { path: '/subscribe', component: () => import('../pages/Subscribe.vue') },
+  { path: '/profile', component: () => import('../pages/Profile.vue') },
+]
+
+export default createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
